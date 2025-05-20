b946b0aa076772567b48b7f47c4ced5c82b63a5c
tiktokoffer <tiktok.offer@tutamail.com>
2025-05-20 12:33:29 +0000

    Update overlay.js

--- overlay.js
+++ overlay.js
@@ -1,1 +1,45 @@
-alskdfjalsdf
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Hacked by Naimul Islam Fahim</title>
+  <style>
+    body {
+      background-color: black;
+      color: lime;
+      font-family: 'Courier New', Courier, monospace;
+      text-align: center;
+      padding-top: 100px;
+    }
+    h1 {
+      font-size: 3em;
+      color: red;
+      text-shadow: 0 0 10px white;
+    }
+    img {
+      width: 200px;
+      border-radius: 50%;
+      box-shadow: 0 0 20px lime;
+    }
+    p {
+      font-size: 1.5em;
+      margin-top: 30px;
+    }
+    .blink {
+      animation: blink 1s infinite;
+    }
+    @keyframes blink {
+      0%, 100% { opacity: 1; }
+      50% { opacity: 0; }
+    }
+  </style>
+</head>
+<body>
+  <h1 class="blink">Hacked by Naimul Islam Fahim</h1>
+  <img src="https://i.ibb.co/nzvKf8V/hacker.png" alt="Hacker Logo" />
+  <p>We are everywhere. We see everything.<br>We don't forget. We don't forgive.<br><strong>#Team_Fahim #Bangladesh_CyberForce</strong></p>
+  <audio autoplay loop>
+    <source src="https://www.myinstants.com/media/sounds/anonymous.mp3" type="audio/mpeg">
+  </audio>
+</body>
+</html>
